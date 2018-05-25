4f8505d12df293bc23d37abfb757b040aac7a23a
kamo(mac) <kamotsuyo@gmail.com>
2018-05-25 17:07:51 +0900

    functions.phpに記述
    とりあえずOK

--- wp-content/themes/kamogashira/functions.php
+++ wp-content/themes/kamogashira/functions.php
@@ -20,3 +20,23 @@ function kamogashira_setup(){
     add_image_size('my-hero',600,300,true);
 }
 add_action('after_setup_theme','kamogashira_setup');
+
+
+//絵文字不要
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+//ブログ投稿機能不要
+remove_action('wp_head', 'rsd_link');
+remove_action('wp_head', 'wlwmanifest_link');
+//WordPressのバージョン表示不要
+remove_action('wp_head', 'wp_generator');
+//Embed 'https://api.w.org/‘ 不要
+remove_action('wp_head','rest_output_link_wp_head');
+//プリフェッチ不要
+function remove_dns_prefetch( $hints, $relation_type ) {
+    if ( 'dns-prefetch' === $relation_type ) {
+        return array_diff( wp_dependencies_unique_hosts(), $hints );
+    }
+    return $hints;
+}
+add_filter( 'wp_resource_hints', 'remove_dns_prefetch', 10, 2 );
